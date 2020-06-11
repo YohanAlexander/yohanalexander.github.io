61c80c2911c114b6fd23e2b0bf62f1326979833c
Yohan Alexander <yohanfranca@gmail.com>
2020-06-11 02:06:44 -0300

    🎨 add cv section

---
+++ _cv/common_variables.R
@@ -0,0 +1,21 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I am a self-taught data scientist, currently majoring in Computer Science at the Federal University of Sergipe. Passionate about the Python ecosystem, open source and astronomical data, I dedicate my time to the study of methods to analyse and extract information from data via computational tools, such as deep learning.
+
+On my academic life, I research at the level of Scientific Initiation in the High Energy Astrophysics Research Group, ASTRalE, in the Physics Department of the Federal University of Sergipe. Founded in 2018, the group has its activities based on space technology and astronomical observations conducted by satellites from American, European and Japanese space agencies.
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "Python",             5,
+  "LaTeX",              4.5,
+  "C/C++",              4,
+  "Bash",               3.5,
+  "Javascript",         3,
+  "SQL",                2.5,
+  "Haskell",            2,
+   "R",                 1,
+)
--- _pages/cv.md
+++ _pages/cv.md
@@ -9,6 +9,6 @@ toc_sticky: true
 
 {% include base_path %}
 
-Also available in [PDF]({{base_path}}/files/cv.pdf).
+CV also available in [PDF]({{base_path}}/files/cv.pdf). You can see my certificates at [DRIVE](https://drive.google.com/drive/folders/1ParksEm2dafW5VAi8u5X7A9FKFeer2kM?usp=sharing).
 
-See my certificates at [DRIVE](https://drive.google.com/drive/folders/1ParksEm2dafW5VAi8u5X7A9FKFeer2kM?usp=sharing).
+ <iframe src="{{base_path}}/files/cv.html" title="CV" height="1100" width="900" frameborder="0" scrolling="auto"></iframe>
