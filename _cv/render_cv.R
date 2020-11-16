c55f402b22ee87eb3eaa33119bf3f0e5abbe7893
Yohan Alexander <yohanfranca@gmail.com>
2020-11-16 17:33:27 -0300

    add cv em portugues

--- _cv/render_cv.R
+++ _cv/render_cv.R
@@ -19,3 +19,19 @@ rmarkdown::render("cv.Rmd",
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "../files/cv.pdf")
+
+# Knit the HTML version
+rmarkdown::render("cv-br.Rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "../files/cv-br.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("cv-br.Rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "../files/cv-br.pdf")
+
